3b57f2497e8f0a25c353de0b9772c712c9a8c942
senicko <25861704+senicko@users.noreply.github.com>
2024-12-15 18:43:01 +0100

    sudoku v10

--- sudoku.py
+++ sudoku.py
@@ -1,14 +1,9 @@
 def digit_ok(board, at_r, at_c, d):
-    # Przechodzimy przez wartosci aktualnego kwadratu
     for r in range(3):
         for c in range(3):
-            # Jezeli w kwadracie jest d, to nie
-            # mozemy go kolejny raz uzyc.
             if board[r + (at_r // 3) * 3][c + (at_c // 3) * 3] == d:
                 return False
 
-    # Sprawdzamy czy w aktualnym wierszu lub
-    # kolumnie jest juz uzyte d.
     for i in range(9):
         if board[at_r][i] == d or board[i][at_c] == d:
             return False
@@ -16,31 +11,21 @@ def digit_ok(board, at_r, at_c, d):
     return True
 
 
-def sudoku_solver(board, r=0, c=0):
-    if r == 9:
+def sudoku_solver(board, i=0):
+    if i == 9 * 9:
         return True
 
-    # Funkcja pomocnicza do okreslania nastepnej pozycji w sudoku
-    def next_position(r, c):
-        if c < 8:
-            return r, c + 1
-        else:
-            return r + 1, 0
+    r, c = i // 9, i % 9
 
-    next_r, next_c = next_position(r, c)
-
-    # Jezeli aktualna komorka ma juz jakas liczbe,
-    # to ja skipujemy
     if board[r][c] != 0:
-        return sudoku_solver(board, next_r, next_c)
+        return sudoku_solver(board, i + 1)
 
-    # Dla kazdej mozliwej wartosci komorki
     for v in range(1, 10):
         if not digit_ok(board, r, c, v):
             continue
 
         board[r][c] = v
-        if sudoku_solver(board, next_r, next_c):
+        if sudoku_solver(board, i + 1):
             return True
         board[r][c] = 0
 
